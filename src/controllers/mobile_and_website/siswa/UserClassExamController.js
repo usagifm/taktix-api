4da56859d75c0506a5d50c3d7a56523540e8d2c7
usagifm <usagifm@gmail.com>
2022-11-30 16:08:42 +0700

    aduh

--- src/controllers/mobile_and_website/siswa/UserClassExamController.js
+++ src/controllers/mobile_and_website/siswa/UserClassExamController.js
@@ -144,6 +144,163 @@ const UserClassExamController = {
         }
     },
 
+    async kontol(req, res, next) {
+        try {
+
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return errorResponse(res, 400, "Validation error", errors.array())
+              }
+
+
+            console.log("Batas : 1")
+            var content = await LksContent.findByPk(req.params.content_id)
+
+            if(!content){
+                return errorResponse(res, 404, "Soal tidak ditemukan", [])
+            }
+
+            console.log("Batas : 2")
+            const NOW = new Date();
+
+            var classMember = await ClassMember.findOne({
+           
+                where: {
+                    class_id: req.params.class_id,
+                    user_id: req.user.user.id
+                }
+            })
+
+            console.log("Batas : 3")
+
+            if(!classMember){
+                return errorResponse(res, 400, "Anda belum masuk ke dalam kelas untuk mengerjakan soal", [])
+            }
+        
+            console.log("Batas : 4")
+            var examAttemption = await LksExamAttemptions.findOne({
+                where: {
+                    lks_content_exam_id: content.id,
+                    user_id: req.user.user.id,
+                    class_id: req.params.class_id,
+                    started_at: {
+                        [Op.lt]: NOW,
+                    },
+                    finished_at: {
+                        [Op.gt]: NOW,
+                    }
+                }
+            })
+            console.log("Batas : 5")
+ 
+       if(examAttemption){
+        console.log("Batas : 6")
+
+        var isAnswered = await LksExamAttemptionsAnswers.findOne({
+            where:{
+                lks_attemption_id: req.body.lks_attemption_id,
+                question_id: req.body.question_id,
+                user_id: req.user.user.id
+            }
+        })
+
+        console.log("Batas : 7")
+
+        if(isAnswered){
+            return errorResponse(res, 400, "Pertanyaan sudah pernah dijawab", [])
+        }
+
+        console.log("Batas : 8")
+        var examQuestion = await LksExamQuestion.findOne({
+            where: {
+                id: req.body.question_id,
+                lks_content_id: content.id
+            }
+        })
+        console.log("Batas : 9")
+
+        if(!examQuestion) {
+            return errorResponse(res, 400, "Pertanyaan tidak ditemukan", [])
+        }
+        console.log("Batas : 10")
+        var isCorrect = 0
+        var isCorrected = 0
+
+        console.log("Batas : 11")
+
+        if (examQuestion.question_type_id == 6001){
+            console.log("Batas : 12")
+
+            if(req.body.answer == examQuestion.answer){
+                isCorrect = 1
+            }
+
+            console.log("Batas : 13")
+            isCorrected = 1
+  
+            var questionAnswer = await LksExamAttemptionsAnswers.create({
+                class_id: req.params.class_id,
+                answer: req.body.answer,
+                lks_attemption_id: req.body.lks_attemption_id,
+                question_id: examQuestion.id,
+                user_id: req.user.user.id,
+                is_correct: isCorrect,
+                is_corrected: isCorrected,
+
+            })
+
+            console.log("Batas : 14")
+        }else {
+            console.log("Batas : 15")
+            var attributes = { 
+                class_id: 2,
+                answer: req.body.answer,
+                lks_attemption_id: req.body.lks_attemption_id,
+                question_id: examQuestion.id,
+                user_id: req.user.user.id,
+                is_correct: isCorrect,
+                is_corrected: isCorrected
+            }
+
+            console.log("Batas : 16")
+
+            if(req.body.image_answer != undefined){
+                attributes.image_answer = req.body.image_answer
+                attributes.answer = "Dijawab dengan gambar"
+            }
+   console.log("Batas : 17")
+
+            var questionAnswer = await LksExamAttemptionsAnswers.create(
+                attributes
+            )
+
+            console.log("Batas : 18")
+            
+        }
+
+                delete questionAnswer.dataValues.is_correct
+                delete questionAnswer.dataValues.is_corrected
+                console.log("Batas : 19")
+                
+                return res.status(200).send({message: "Pertanyaan berhasil dijawab", data: questionAnswer})
+     
+        
+       }else {
+
+        return errorResponse(res, 400, "Attemption sudah tidak aktif atau tidak ditemukan", [])
+
+       }
+
+        } catch (error) {
+            console.log(error)
+            let errStacks = []
+            if (error.errors) {
+                errStacks = errorMapper(error.errors)
+            }
+            return errorResponse(res, 400, error.message, errStacks)
+        }
+    },
+
     async insertAnswerLksExamQuestion(req, res, next) {
         try {
 
