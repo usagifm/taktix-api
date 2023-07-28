eff816c20a3f10b9fb6295cb9c1f494baae270d1
muhammadfaqqih <muhammad.faqqih@visionplus.id>
2023-07-28 16:09:42 +0700

    add new migration for incorrect_statement_label and correct_statement_label

---
+++ src/db/migrations/20231865325291-add-new-question-type-to-set-masters.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    queryInterface.bulkInsert('set_masters', [
+      {
+          id: 6004,
+          category: 'question_type',
+          name: 'Pilihan Ganda Kompleks',
+          created_at: new Date(),
+          updated_at: new Date(),
+      },
+      {
+          id: 6005,
+          category: 'question_type',
+          name: 'Pernyataan Ganda',
+          created_at: new Date(),
+          updated_at: new Date(),
+      }])
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    queryInterface.bulkDelete('set_masters', {
+      [Op.or]: [
+          { id: 6004 },
+          { id: 6005 }
+      ],
+  })
+
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
